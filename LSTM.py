0529f06feee827c22efdbd2127c14a2d9b97a5ac
exaintelligence <exaintelligence@gmail.com>
2016-05-02 18:42:04 +0900

    optimizers

--- LSTM.py
+++ LSTM.py
@@ -19,17 +19,17 @@ class LSTM(chainer.Chain):
     def __init__(self, length, n_outputs, n_units=256, train=True):
         super(LSTM, self).__init__(
             l0=L.Linear(length, n_units),
-            l1=L.LSTM(n_units, n_units),
-            l2=L.LSTM(n_units, n_units),
-            l3=L.Linear(n_units,n_outputs)
+            l1=L.LSTM(n_units, n_outputs),
+            #l2=L.LSTM(n_units, n_outputs),
+            #l3=L.Linear(n_units,n_outputs)
         )
 
     def __forward(self, x):
         h0 = self.l0(x)
         h1 = self.l1(h0)
-        h2 = self.l2(h1)
-        h3 = self.l3(h2)
-        return h3
+        #h2 = self.l2(h1)
+        #h3 = self.l3(h2)
+        return h1
 
     def forward(self, x_data, y_data, train=True, gpu=-1):
         if gpu >= 0:
@@ -43,7 +43,7 @@ class LSTM(chainer.Chain):
 
     def reset_state(self):
         self.l1.reset_state()
-        self.l2.reset_state()
+        #self.l2.reset_state()
 
     def predict(self, x_data, gpu=-1):
         if gpu >= 0:
@@ -91,10 +91,10 @@ class LRCN:
 
                 t = np.asarray([randomMotion], dtype=np.int32)
 
+                self.optimizer.zerograds()
                 loss, acc = self.model.forward(x, t, gpu=self.gpu)
                 loss.backward()
                 self.optimizer.update
-                self.model.zerograds()
 
                 sum_train_loss += float(cuda.to_cpu(loss.data))
                 sum_train_accuracy += float(cuda.to_cpu(acc.data))
@@ -120,7 +120,6 @@ class LRCN:
                     sum_test_loss += float(cuda.to_cpu(loss.data))
                     sum_test_accuracy += float(cuda.to_cpu(acc.data))
                 print 'test mean loss={}, accuracy={}'.format(sum_test_loss/len(sequence), sum_test_accuracy/len(sequence))
-                print '=================================='
             
             self.model.reset_state()
 
@@ -132,9 +131,10 @@ class LRCN:
                 for i, image in enumerate(sequence):
                     x = image[np.newaxis, :]
                     result = cuda.to_cpu(self.model.predict(x, gpu=self.gpu))
-                    prob = prob[0] + result[0]/len(sequence-1)
+                    prob = prob[0] + result[0]/len(sequence)
                 print 'Answer:', randomMotion, ' Pred:', np.argmax(prob), ',',np.max(prob)*100,'%'
                 print 'prob: ', prob
+                print '=================================='
 
             epoch += 1
 
